e8ac39c1d8e63366bd416674b5612f55ca850da2
Axel Forsman <axelsfor@gmail.com>
2018-02-06 21:42:07 +0100

    Query 3 weeks in advance.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,142 +12,115 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
 import java.util.List;
-import java.util.function.*;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
-	static final String URL_NAME = "https://web.skola24.se/timetable/timetable-viewer/data/render";
-
-	static final String[] weekdays = { "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag" };
+	private static final String URL_NAME = "https://web.skola24.se/timetable/timetable-viewer/data/render";
+	private static final String[] weekdays = {"Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"};
+	static final Pattern timeRegex = Pattern.compile("^(\\d{1,2}):(\\d{2})$");
+	static final Predicate<String> notTime = timeRegex.asPredicate().negate();
 
 	public static void main(String[] args) throws IOException {
+		Calendar calendar = Calendar.getInstance();
+		List<VEvent> events = new ArrayList<>();
+		for (int week = calendar.get(Calendar.WEEK_OF_YEAR), i = 0; i < 3; week = (week + 1) % 52, ++i) {
+			System.out.println("---- Week " + week + " -----");
+			JSONObject data = getJsonData(week);
+			List<Lesson> lessons = parseLessons(data);
+			events.addAll(getEventsFromLessons(lessons, week));
+		}
+		String calString = genICalendarFromEvents(events);
+		try (PrintStream ps = new PrintStream(new FileOutputStream("cal.ics"))) {
+			ps.print(calString);
+		}
+	}
+
+	private static JSONObject getJsonData(int week) throws IOException {
 		URL url = new URL(URL_NAME);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setDoInput(true);
 		con.setDoOutput(true);
 		con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-		con.setRequestProperty("Accept", "application/json; q=0.01");
+		con.setRequestProperty("Accept", "application/json");
 		con.setRequestMethod("POST");
-		con.setRequestProperty("Host", "web.skola24.se");
-		con.setRequestProperty("Referer", "https://web.skola24.se/timetable/timetable-viewer/harryda.skola24.se/Huleb%C3%A4cksgymnasiet/class/NA15A/");
-		con.setRequestProperty("Cookie", "ASP.NET_SessionId=2g45y51ncq352vkvxlacnca2");
 
 		JSONObject param = new JSONObject();
 		param.put("divWidth", 1500);
 		param.put("divHeight", 550);
 		param.put("domain", "harryda.skola24.se");
 		param.put("headerEnabled", false);
-		param.put("selectedWeek", 5);
+		param.put("selectedWeek", week);
 
 		JSONObject selectedSchool = new JSONObject();
 		selectedSchool.put("guid", "219254f7-917c-472e-ac23-6982156c6164");
-		selectedSchool.put("name", "Hulebäcksgymnasiet");
+		// selectedSchool.put("name", "Hulebäcksgymnasiet");
 		param.put("selectedSchool", selectedSchool);
 
-		JSONObject selectedClass = new JSONObject();
-		selectedClass.put("guid", "c203b24e-de7d-4719-9258-e69500800523");
-		selectedClass.put("id", "NA15A");
-		selectedClass.put("isClass", true);
-		param.put("selectedClass", selectedClass);
+		// JSONObject selectedClass = new JSONObject();
+		// selectedClass.put("guid", "c203b24e-de7d-4719-9258-e69500800523");
+		// selectedClass.put("id", "NA15C");
+		// selectedClass.put("isClass", true);
+		// param.put("selectedClass", selectedClass);
 
-		OutputStream os = con.getOutputStream();
-		os.write(param.toString().getBytes("UTF-8"));
-		os.close();
+		JSONObject selectedSignatures = new JSONObject();
+		selectedSignatures.put("signature", "990811-6073");
+		param.put("selectedSignatures", selectedSignatures);
+
+		try (OutputStream os = con.getOutputStream()) {
+			os.write(param.toString().getBytes("UTF-8"));
+		}
 
 		int result = con.getResponseCode();
 		switch (result) {
 			case HttpURLConnection.HTTP_OK:
-				BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-				StringBuilder builder = new StringBuilder();
-				String line;
-				while ((line = br.readLine()) != null) {
-					builder.append(line).append('\n');
+				try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"))) {
+					StringBuilder builder = new StringBuilder();
+					String line;
+					while ((line = br.readLine()) != null) {
+						builder.append(line).append('\n');
+					}
+					JSONObject response = new JSONObject(builder.toString());
+					return response.getJSONObject("data");
 				}
-				br.close();
-				JSONObject response = new JSONObject(builder.toString());
-				JSONObject data = response.getJSONObject("data");
-				List<Lesson> lessons = parseLessons(data);
-				String calString = genLessonsCalendar(lessons);
-				try (PrintStream ps = new PrintStream(new FileOutputStream("cal.ics"))) {
-					ps.print(calString);
-				}
-
-				break;
 			default:
 				System.out.println(con.getResponseMessage());
+				return null;
 		}
 	}
 
-	private static String genLessonsCalendar(List<Lesson> lessons) {
-		ICalendar ical = new ICalendar();
+	private static List<VEvent> getEventsFromLessons(List<Lesson> lessons, int week) {
+		final Calendar c = Calendar.getInstance();
+		c.set(Calendar.WEEK_OF_YEAR, week);
+		c.set(Calendar.SECOND, 0);
 
-		lessons.stream().map(lesson -> {
+		return lessons.stream().map(lesson -> {
 			VEvent event = new VEvent();
 			Summary summary = event.setSummary(lesson.text);
 			summary.setLanguage("sv-SE");
 			event.setColor(new Color(lesson.color));
 
-			Calendar c1 = Calendar.getInstance();
-			c1.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY + lesson.dayIndex);
-			c1.set(Calendar.HOUR_OF_DAY, lesson.start.hour);
-			c1.set(Calendar.MINUTE, lesson.start.minute);
-			c1.set(Calendar.SECOND, 0);
-			event.setDateStart(c1.getTime(), true);
+			c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY + lesson.dayIndex);
 
-			Calendar c2 = Calendar.getInstance();
-			c2.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY + lesson.dayIndex);
-			c2.set(Calendar.HOUR_OF_DAY, lesson.end.hour);
-			c2.set(Calendar.MINUTE, lesson.end.minute);
-			c1.set(Calendar.SECOND, 0);
-			event.setDateEnd(c2.getTime(), true);
+			c.set(Calendar.HOUR_OF_DAY, lesson.start.hour);
+			c.set(Calendar.MINUTE, lesson.start.minute);
+			event.setDateStart(c.getTime(), true);
+
+			c.set(Calendar.HOUR_OF_DAY, lesson.end.hour);
+			c.set(Calendar.MINUTE, lesson.end.minute);
+			event.setDateEnd(c.getTime(), true);
 
 			return event;
-		}).forEach(ical::addEvent);
+		}).collect(Collectors.toList());
+	}
 
+	private static String genICalendarFromEvents(List<VEvent> events) {
+		ICalendar ical = new ICalendar();
+		events.forEach(ical::addEvent);
 		return Biweekly.write(ical).go();
-	}
-
-	static class Text {
-		final int x, y;
-		final String fcolor;
-		final double fontsize;
-		final int id;
-		final String text;
-		// boolean bold, italic;
-
-		Text(JSONObject obj) {
-			x = obj.getInt("x");
-			y = obj.getInt("y");
-			fcolor = obj.getString("fcolor");
-			fontsize = obj.getDouble("fontsize");
-			id = obj.getInt("id");
-			text = obj.getString("text");
-		}
-	}
-
-	static class Box {
-		int x, y, width, height;
-		String bcolor, fcolor;
-		int id;
-		String type;
-
-		Box(JSONObject obj) {
-			x = obj.getInt("x");
-			y = obj.getInt("y");
-			width = obj.getInt("width");
-			height = obj.getInt("height");
-			bcolor = obj.getString("bcolor");
-			fcolor = obj.getString("fcolor");
-			id = obj.getInt("id");
-			type = obj.getString("type");
-		}
-
-		boolean contains(Point p) {
-			return p.x >= x && p.x < x + width && p.y >= y && p.y < y + height;
-		}
 	}
 
 	public static <T> Collector<T, ?, T> singletonCollector() {
@@ -162,57 +135,20 @@ public class Main {
 		);
 	}
 
-	static final Pattern timeRegex = Pattern.compile("^(\\d{1,2}):(\\d{2})$");
-	static final Predicate<String> notTime = timeRegex.asPredicate().negate();
-
-	static class Time {
-		final int hour, minute;
-
-		Time(int hour, int minute) {
-			this.hour = hour;
-			this.minute = minute;
-		}
-
-		Time(String s) {
-			Matcher m = timeRegex.matcher(s);
-			m.find();
-			hour = Integer.valueOf(m.group(1));
-			minute = Integer.valueOf(m.group(2));
-		}
-
-		@Override
-		public String toString() {
-			return String.format("%d:%02d", hour, minute);
-		}
-	}
-
-	enum LessonTime {
-		START, END
-	}
-
-	private static class WeightedText {
-		final Text text;
-		final int weight;
-
-		public WeightedText(Text text, int weight) {
-			this.text = text;
-			this.weight = weight;
-		}
-	}
-
 	/**
 	 * Finds the time stamps for a lesson.
+	 *
 	 * @param texts
-	 * @param box The containing box of the lesson.
+	 * @param box   The containing box of the lesson.
 	 * @param type
 	 * @return
 	 */
-	static Time findTimeFromPoint(List<Text> texts, Box box, LessonTime type) {
+	private static Time findTimeFromPoint(List<Text> texts, Box box, LessonTime type) {
 		Point p = type == LessonTime.START ?
 				new Point(box.x, box.y)
 				: new Point(box.x + box.width, box.y + box.height);
 
-		return new Time(texts.stream()
+		Optional<WeightedText> timeText = texts.stream()
 				// Only consider time stamps
 				.filter(text -> timeRegex.asPredicate().test(text.text))
 				.map(text -> {
@@ -225,19 +161,14 @@ public class Main {
 							// Any difference should only come from centering the label
 							+ 5 * (int) Math.pow(Math.abs(dy), 3);
 
-					// System.out.println("score: " + score + " time: " + text.text + " dx: " + dx + " dy: " + dy);
+					// System.out.println("score: " + score + " time: " + timeText.timeText + " dx: " + dx + " dy: " + dy);
 
 					return new WeightedText(text, score);
 				})
 				.sorted(Comparator.comparingInt(a -> a.weight))
-				.findFirst().get().text.text);
-	}
-
-	static class Lesson {
-		String text;
-		int dayIndex;
-		Time start, end;
-		String color;
+				.findFirst();
+		if (!timeText.isPresent()) throw new AssertionError();
+		return new Time(timeText.get().text.text);
 	}
 
 	private static List<Lesson> parseLessons(JSONObject data) {
@@ -303,5 +234,87 @@ public class Main {
 		}
 
 		return lessons;
+	}
+
+	enum LessonTime {
+		START, END
+	}
+
+	static class Text {
+		final int x, y;
+		final String fcolor;
+		final double fontsize;
+		final int id;
+		final String text;
+		// boolean bold, italic;
+
+		Text(JSONObject obj) {
+			x = obj.getInt("x");
+			y = obj.getInt("y");
+			fcolor = obj.getString("fcolor");
+			fontsize = obj.getDouble("fontsize");
+			id = obj.getInt("id");
+			text = obj.getString("text");
+		}
+	}
+
+	static class Box {
+		int x, y, width, height;
+		String bcolor, fcolor;
+		int id;
+		String type;
+
+		Box(JSONObject obj) {
+			x = obj.getInt("x");
+			y = obj.getInt("y");
+			width = obj.getInt("width");
+			height = obj.getInt("height");
+			bcolor = obj.getString("bcolor");
+			fcolor = obj.getString("fcolor");
+			id = obj.getInt("id");
+			type = obj.getString("type");
+		}
+
+		boolean contains(Point p) {
+			return p.x >= x && p.x < x + width && p.y >= y && p.y < y + height;
+		}
+	}
+
+	static class Time {
+		final int hour, minute;
+
+		Time(int hour, int minute) {
+			this.hour = hour;
+			this.minute = minute;
+		}
+
+		Time(String s) {
+			Matcher m = timeRegex.matcher(s);
+			if (!m.find()) throw new AssertionError();
+			hour = Integer.valueOf(m.group(1));
+			minute = Integer.valueOf(m.group(2));
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%d:%02d", hour, minute);
+		}
+	}
+
+	private static class WeightedText {
+		final Text text;
+		final int weight;
+
+		WeightedText(Text text, int weight) {
+			this.text = text;
+			this.weight = weight;
+		}
+	}
+
+	static class Lesson {
+		String text;
+		int dayIndex;
+		Time start, end;
+		String color;
 	}
 }
